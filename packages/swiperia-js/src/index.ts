224bf31d913be636fdcfe50a570a16b6074b506c
Ehsan Samavati <samaavaati@gmail.com>
2024-04-13 11:13:21 +0300

    chore(js): update index exports for swipe detection classes
    Updated the main index file of the swiperia-js package to export the newly created classes: AbstractSwipeDetector, MouseSwipeDetector, TouchSwipeDetector, and SwipeDetector. This change facilitates easy importation and usage of swipe detection functionalities within external applications.

---
+++ packages/swiperia-js/src/index.ts
@@ -0,0 +1,4 @@
+export { AbstractSwipeDetector } from './lib/AbstractSwipeDetector';
+export { MouseSwipeDetector } from './lib/MouseSwipeDetector';
+export { TouchSwipeDetector } from './lib/TouchSwipeDetector';
+export { SwipeDetector } from './lib/SwipeDetector';
