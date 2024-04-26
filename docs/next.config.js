d7a3b70b81fae2a5ff494eb4e1637af3075aecf3
Ehsan Samavati <samaavaati@gmail.com>
2024-04-27 00:36:53 +0300

    feat(docs): :memo: docs setup added

---
+++ docs/next.config.js
@@ -0,0 +1,40 @@
+//@ts-check
+
+/**
+ * we need this to fix the issue with integrating the nx with nextra
+ */
+if (process.cwd() !== __dirname) {
+  process.chdir(__dirname);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { composePlugins, withNx } = require('@nx/next');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextra = require('nextra');
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  nx: {
+    // Set this to true if you would like to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
+  },
+};
+
+// @ts-ignore
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+});
+
+const plugins = [
+  // Add more Next.js plugins to this list if needed.
+  withNx,
+  withNextra,
+];
+
+const composed = composePlugins(...plugins)(nextConfig);
+
+module.exports = composed;
---
+++ docs/theme.config.tsx
@@ -0,0 +1,12 @@
+import { type ThemeConfig } from 'nextra';
+
+const config: ThemeConfig = {
+  logo: <span>Swiperia</span>,
+  project: {
+    link: 'https://github.com/samavati/swiperia',
+
+  },
+};
+
+export default config;
+
