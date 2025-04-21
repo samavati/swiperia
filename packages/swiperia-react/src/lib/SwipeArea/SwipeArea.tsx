e6cb6b6add002dc93b0037401872dc7452d9f250
Ehsan Samavati <samaavaati@gmail.com>
2025-04-21 16:18:03 +0300

    feat(swiperia-react): add onSwipeCancelled callback and update documentation
    This commit introduces the `onSwipeCancelled` callback to the `SwiperiaCallbacks` interface, allowing users to handle swipe cancellation events. Additionally, the documentation for the `SwipeArea` component and the `useSwiperia` hook has been updated to reflect this new functionality, ensuring users have clear guidance on its usage.

--- packages/swiperia-react/src/lib/SwipeArea/SwipeArea.tsx
+++ packages/swiperia-react/src/lib/SwipeArea/SwipeArea.tsx
@@ -20,6 +20,7 @@ const SwipeArea = forwardRef<HTMLDivElement, SwipeAreaProps>(
       onSwipedRight,
       onSwipedUp,
       onSwiping,
+      onSwipeCancelled,
       ...props
     },
     ref
@@ -32,6 +33,7 @@ const SwipeArea = forwardRef<HTMLDivElement, SwipeAreaProps>(
       onSwipedRight,
       onSwipedUp,
       onSwiping,
+      onSwipeCancelled,
     });
 
     const handleRef = useCallback(
